822f93023accb55a19bfe4382d3cd6eb41c25303
bear <blueworrybear@gmail.com>
2012-02-21 18:41:57 +0800

    Initial Commit

---
+++ AperireSchola/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AperireSchola
+//
+//  Created by bear on 12/2/21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AperireSchola/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AperireSchola
+//
+//  Created by bear on 12/2/21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
