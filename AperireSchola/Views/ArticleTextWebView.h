27eda9e7de20e2ccf20cc269256cd137c4e14b6b
bear <blueworrybear@gmail.com>
2012-03-01 16:41:51 +0800

    Have to modify: 1.BannerView -> Using builder pattern to make it reusable. 2.ArticleView

---
+++ AperireSchola/Views/ArticleTextWebView.h
@@ -0,0 +1,13 @@
+//
+//  ArticleTextWebView.h
+//  AperireSchola
+//
+//  Created by bear on 12/3/1.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticleTextWebView : UIWebView <UIWebViewDelegate>
+
+@end
